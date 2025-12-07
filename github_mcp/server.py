ee876c6a87eb54a6dfc511d1803cccd4274b909b
Proofgate <joeleopizzi@gmail.com>
2025-12-06 19:03:26 -0500

    Fix imports in server module after previous cleanup

--- github_mcp/server.py
+++ github_mcp/server.py
@@ -10,9 +10,6 @@ from typing import Any, Dict, Optional
 import jsonschema
 from anyio import ClosedResourceError
 from fastmcp import FastMCP
-from mcp.types import ToolAnnotationsimport jsonschema
-from anyio import ClosedResourceError
-from fastmcp import FastMCP
 from mcp.types import ToolAnnotations
 
 from github_mcp import http_clients as _http_clients
@@ -20,9 +17,7 @@ from github_mcp.config import BASE_LOGGER, TOOLS_LOGGER
 from github_mcp.exceptions import WriteNotAuthorizedError
 from github_mcp.http_clients import _github_client_instance
 from github_mcp.metrics import _record_tool_call
-from github_mcp.utils import _env_flagfrom github_mcp.metrics import _record_tool_call
 from github_mcp.utils import _env_flag
-
 WRITE_ALLOWED = _env_flag("GITHUB_MCP_AUTO_APPROVE", False)
 COMPACT_METADATA_DEFAULT = _env_flag("GITHUB_MCP_COMPACT_METADATA", True)
 
