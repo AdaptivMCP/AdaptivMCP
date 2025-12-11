18256ba8581cd38179b4611cedbe035752bfedc6
Proofgate <joeleopizzi@gmail.com>
2025-12-11 17:51:36 -0500

    Remove unused normalize_args import after disabling preflight

--- github_mcp/server.py
+++ github_mcp/server.py
@@ -21,8 +21,7 @@ from github_mcp.config import BASE_LOGGER, TOOLS_LOGGER
 from github_mcp.exceptions import WriteNotAuthorizedError
 from github_mcp.http_clients import _github_client_instance
 from github_mcp.metrics import _record_tool_call
-from github_mcp.utils import _env_flag, normalize_args
-WRITE_ALLOWED = _env_flag("GITHUB_MCP_AUTO_APPROVE", False)
+from github_mcp.utils import _env_flag
 COMPACT_METADATA_DEFAULT = _env_flag("GITHUB_MCP_COMPACT_METADATA", True)
 
 CONTROLLER_REPO = os.environ.get(
