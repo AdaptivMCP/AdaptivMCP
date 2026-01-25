b9a48804d7f537086fbe6fdbf1f802bc8836c521
Proofgate <joeleopizzi@gmail.com>
2026-01-25 04:29:44 -0500

    Delete tests/test_provider_logs_non_json.py

--- tests/test_provider_logs_non_json.py
+++
@@ -1,7 +0,0 @@
-def test_truncate_text_does_not_json_dump_mappings():
-    from github_mcp.mcp_server import decorators
-
-    s = decorators._truncate_text({"b": 2, "a": 1})
-    assert "a=1" in s
-    assert "b=2" in s
-    assert "{" not in s
